3870049f323507bb80e4baaaa71c2394718d3ef8
catrone3 <catrone3@gmail.com>
2024-12-26 22:40:44 +0000

    Last little changes

--- start_services.sh
+++ start_services.sh
@@ -6,10 +6,10 @@ echo "PocketBase URL: ${POCKETBASE_URL}"
 
 cp -r /app/whisper.cpp /models/
 # Start PocketBase in the background
-#pocketbase serve --http=0.0.0.0:8080 --dir /app/db &
+# pocketbase serve --http=0.0.0.0:8080 --dir /app/db &
 
 # Create the admin user using environment variables
-#pocketbase admin create "${POCKETBASE_ADMIN_EMAIL}" "${POCKETBASE_ADMIN_PASSWORD}" --dir /app/db
+# pocketbase admin create "${POCKETBASE_ADMIN_EMAIL}" "${POCKETBASE_ADMIN_PASSWORD}" --dir /app/db
 
 npm run build
 node build
